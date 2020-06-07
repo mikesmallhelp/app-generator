6b83dc2683a27a9b2d4a0bea99b0c5d78af909cd
Mike <mikesmallhelp@gmail.com>
2020-06-07 10:43:06 +0300

    Generate app html

--- app-generator.py
+++ app-generator.py
@@ -1,7 +1,22 @@
 import os
 import app_data
 
+def generateAppComponentHtml(appData):
+   print('Start generating app component html')
+
+   try:
+      f = open('src/app/app.component.html', 'w')
+      f.write('<H1>'+ appData.appName  +'</H1>' + '\n')
+      f.write('<app-' + appData.cardName + '></app-' + appData.cardName + '>'); 
+
+   finally:
+      f.close()
+
+   print('End generating app component html')
+
 appData = app_data.readAppData()
 os.system('ng new ' + appData.appName + ' --routing=false --style=scss')
 os.chdir(appData.appName)
 os.system('ng generate component ' + appData.cardName)
+generateAppComponentHtml(appData)
+os.system('ng serve')
